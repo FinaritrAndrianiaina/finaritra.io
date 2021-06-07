dd76a20e9262d905d9bf6e492fb6951c9325809e
FinaritrAndrianiaina <finaritrandrianiaina@gmail.com>
2021-06-07 15:39:41 +0300

    fix sitemap generator

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -15,12 +15,13 @@ const prettier = require("prettier");
   ]);
 
   const sitemap = `
-        <?xml version="1.0" encoding="UTF-8"?>
+        <?xml version="1.0" encoding="UTF-8">
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
             ${pages
               .map((page) => {
                 const path = page
                   .replace("pages", "")
+                  .replace("md", "")
                   .replace(".js", "")
                   .replace(".tsx", "")
                   .replace(".mdx", "");
