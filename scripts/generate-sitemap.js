105ed7b6e60c109f0e1d1a0e278f1cdf53b8dccf
FinaritrAndrianiaina <finaritrandrianiaina@gmail.com>
2021-06-09 09:06:35 +0300

    sitemap update

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -15,7 +15,7 @@ const prettier = require("prettier");
   ]);
 
   const sitemap = `
-        <?xml version="1.0" encoding="UTF-8">
+        <?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
             ${pages
               .map((page) => {
